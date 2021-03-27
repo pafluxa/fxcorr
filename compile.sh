f3613406f60da8ce612c7ba08aff06a024a69d00
Pedro Fluxa <pfluxa@desertpointtech.com>
2021-03-27 12:13:14 -0300

    added fftw tests

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 nvcc -c fxcorr.cu cufft_routines.cu
 g++ -c main.c -I /usr/local/cuda/include
-g++ -o test.exe main.o fxcorr.o cufft_routines.o -I/usr/local/cuda/include -L/usr/local/cuda/lib64 -lcuda -lcudart -lcufft
+g++ -o test.exe main.o fxcorr.o cufft_routines.o -I/usr/local/cuda/include -L/usr/local/cuda/lib64 -lcuda -lcudart -lcufft -lfftw3_omp -lfftw3 -lm
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <time.h>
 #include <stdio.h>
+#include <fftw3.h>
 
 #include "fxcorr.cuh"
 
@@ -21,21 +22,68 @@ int main(void)
         s2[2*i + 1] = 0.0;
     }
 
-    clock_t t;
-    t = clock();
-
     fxcorr_attach_signals(s1, s2, nsamples, fxd);
     fxcorr_allocate_and_zeropad(fxd);
     fxcorr_create_cufft_handle(fxd);
-    fxcorr_compute(fxd);\
+    clock_t t;
+    t = clock();
+    fxcorr_compute(fxd);
     // uncomment to print result to stdout
     //for(long i=0; i<nsamples; i++){printf("%06ld%10.2E\n", i, fxd->out[i]);}
     fxcorr_deallocate(fxd);
-
+    
     t = clock() - t;
     // calculate the elapsed time
     double time_taken = ((double)t)/CLOCKS_PER_SEC; 
-    printf("correlation took %f seconds to execute\n", time_taken);
+    printf("correlation took %f seconds on GPU\n", time_taken);
+    
+    fftw_complex* ss1 = fftw_alloc_complex(nsamples);
+    fftw_complex* ss2 = fftw_alloc_complex(nsamples);
+    fftw_complex* sr1 = fftw_alloc_complex(nsamples);
+    fftw_complex* sr2 = fftw_alloc_complex(nsamples);
+
+    fftw_plan_with_nthreads(16);
+    fftw_plan plan1 = fftw_plan_dft_1d(
+        nsamples,
+        ss1,
+        sr1,
+        FFTW_FORWARD,
+        FFTW_ESTIMATE);
+
+    fftw_plan plan2 = fftw_plan_dft_1d(
+        nsamples,
+        ss2,
+        sr2,
+        FFTW_FORWARD,
+        FFTW_ESTIMATE);
+
+    fftw_plan plan3 = fftw_plan_dft_1d(
+        nsamples,
+        sr2,
+        ss2,
+        FFTW_BACKWARD,
+        FFTW_ESTIMATE);
+
+    for(long i=0; i<nsamples; i++)
+    {
+        ss1[i][0] = s1[2*i];
+        ss1[i][1] = s1[2*i+1];
+        
+        ss2[i][0] = s2[2*i];
+        ss2[i][1] = s2[2*i + 1];
+    }
+    t = clock();
+    fftw_execute(plan1);
+    fftw_execute(plan2);
+    fftw_execute(plan3);
+    t = clock() - t;
+    // calculate the elapsed time
+    time_taken = ((double)t)/CLOCKS_PER_SEC; 
+    printf("correlation took %f seconds on CPU\n", time_taken);
+
+    fftw_destroy_plan(plan1);
+    fftw_destroy_plan(plan2);
+    fftw_destroy_plan(plan3);
     
     
     return 0;
